5e6f07a2e1c56d6d39b36074a4379f5a6a229633
LuoRain <luotianyi@luotianyi.me>
2021-06-22 23:08:31 +0800

    Fix CancelablePromise, fix #27

--- src/typescript/modules/cancelable-promise.ts
+++ src/typescript/modules/cancelable-promise.ts
@@ -6,6 +6,7 @@ export default class CancelablePromise<T> extends Promise<T> {
     }
 
     cancel() {
-        this.cancelCallback();
+        if (typeof this.cancelCallback === "function")
+            this.cancelCallback();
     }
 }
