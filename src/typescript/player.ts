a2abb69bbe3577f241e787a53e8a3dc4ab6baafe
LuoRain <luotianyi@luotianyi.me>
2021-06-22 23:45:32 +0800

    Style update, no autoplay for cellular network

--- src/typescript/player.ts
+++ src/typescript/player.ts
@@ -64,6 +64,10 @@ function initialize(options: string | PenguinPlayerOptions) {
                 options: playerOptions.playlist
             }
         ]
+    // Disable autoplay if using slow network or cellular network
+    let connectionInfo = ((<any>navigator).connection || (<any>navigator).mozConnection || (<any>navigator).webkitConnection);
+    if (typeof playerOptions.autoplay != "boolean" && connectionInfo)
+        playerOptions.autoplay = connectionInfo.effectiveType !== "slow-2g" && (!connectionInfo.type || connectionInfo.type !== "cellular");
     let waitPromises: Promise<Song[]>[] = [];
     for (let playlist of playerOptions.playlist)
         waitPromises.push(getProvider(playlist.type).getPlaylist(playlist.options));
