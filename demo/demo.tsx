e669de56a2f2e5cf251b5d7dd58482437987af97
LuoRain <luotianyi@luotianyi.me>
2022-03-20 09:33:14 +0800

    fix: pplayer demo detection
    Signed-off-by: LuoRain <luotianyi@luotianyi.me>

--- demo/demo.tsx
+++ demo/demo.tsx
@@ -10,7 +10,11 @@ declare const PPlayer: any;
 render(<App/>, document.body);
 
 window.addEventListener("load", () => {
-    if (!PPlayer || PPlayer.version != _VERSION_) {
-        alert("Incompatible Penguin Player version, this demo is built for Penguin Player v" + _VERSION_ + ", but version " + PPlayer?.version + " is detected!");
+    if ('PPlayer' in window) {
+        if (PPlayer.version != _VERSION_) {
+            alert("Incompatible Penguin Player version, this demo is built for Penguin Player v" + _VERSION_ + ", but version " + PPlayer?.version + " is detected!");
+        }
+    } else {
+        alert("Penguin Player not detected, is it fail to load or it contains some fatal error?");
     }
 });
