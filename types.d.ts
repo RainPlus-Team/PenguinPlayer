1c39b2ffd0c3dec302f3b4f33a18f91a4caa6ea1
LuoRain <luotianyi@luotianyi.me>
2022-03-23 13:32:11 +0800

    remove: unused types

--- types.d.ts
+++ types.d.ts
@@ -12,22 +12,9 @@ interface PenguinPlayerOptions {
     parent?: HTMLElement
     fixed?: boolean
     volume?: number
-    autoplay?: boolean
+    autoplay?: boolean | "auto"
     song?: number
     theme?: new () => Theme
-}
-
-declare class Color extends Array {
-    0: number
-    1: number
-    2: number
-}
-
-interface SliderOptions {
-    activeSelector: string
-    barSelector: string
-    innerSelector: string
-    value?: number
 }
 
 interface PPlayerLayoutState {
@@ -37,7 +24,4 @@ interface PPlayerLayoutState {
 interface PPlayerLayoutProps {
     options: PenguinPlayerOptions
     player: import("./src/ts/player").Player
-    currentTime: number
-    duration: number
-    song: import("./src/ts/player").Song
 }
