05f78a38c0dc62bd1771ddfcc945f5873d28a2a7
LuoRain <luotianyi@luotianyi.me>
2022-03-23 13:31:51 +0800

    add: usage notes

--- demo/examples/simple.js
+++ demo/examples/simple.js
@@ -1,7 +1,26 @@
 // Concept code, might be changed in future
-PPlayer.initialize([ // Initialize with an array of playlist
+
+// First, make sure you know how to use Penguin Player's exports
+// All functions or properties used below will be simplified to export name
+
+// If you are using standalone JavaScript file, it will be exposed under
+window.PPlayer
+// And to access initialize function, just
+PPlayer.initialize(/* options */);
+
+// If you are using npm package, you can import it like
+import PPlayer from "@m4tec/penguinplayer";
+PPlayer.initialize(/* options */);
+// or
+import { initialize } from "@m4tec/penguinplayer";
+initialize(/* options */);
+
+
+// The simplest way to use it is give a list of playlist to player
+initialize([ // Initialize with an array of playlist
     { // Playlist
         provider: "netease", // Playlist provider (which means music platform)
         id: "[NETEASE CLOUD MUSIC PLAYLIST ID]" // Platform's playlist metadata
     }
 ]);
+// That's it! You are getting a fixed (float on page) player!
