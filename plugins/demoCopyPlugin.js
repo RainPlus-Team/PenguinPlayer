a5ea4e09a96ede8ba693828596b8622d193749bd
LuoRain <luotianyi@luotianyi.me>
2022-03-25 22:53:52 +0800

    fix: no such dir when first build
    Signed-off-by: LuoRain <luotianyi@luotianyi.me>

--- plugins/demoCopyPlugin.js
+++ plugins/demoCopyPlugin.js
@@ -3,13 +3,16 @@ const path = require("path");
 
 class DemoCopyPlugin {
     apply(compiler) {
-        compiler.hooks.done.tap("DemoCopyPlugin", (stats) => {
-            const files = Object.keys(stats.compilation.assets);
-            for (let file of files) {
-                const p = path.resolve(stats.compilation.compiler.outputPath, file);
-                const target = path.resolve(stats.compilation.compiler.outputPath, "demo", file);
-                fs.copyFileSync(p, target, fs.constants.COPYFILE_FICLONE);
+        compiler.hooks.assetEmitted.tap("DemoCopyPlugin", (file, {compilation}) => {
+            const p = path.resolve(compilation.compiler.outputPath, file);
+
+            const demo = path.resolve(compilation.compiler.outputPath, "demo");
+            if (!fs.existsSync(demo)) {
+                fs.mkdirSync(demo);
             }
+
+            const target = path.resolve(compilation.compiler.outputPath, "demo", file);
+            fs.copyFileSync(p, target, fs.constants.COPYFILE_FICLONE);
         });
     }
 }
