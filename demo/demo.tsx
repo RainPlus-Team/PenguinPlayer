7976d1bd6a856b2927f2df890bfc731ec5b5a700
LuoRain <luotianyi@luotianyi.me>
2022-04-08 21:56:20 +0800

    chg: demo code style
    Signed-off-by: LuoRain <luotianyi@luotianyi.me>

--- demo/demo.tsx
+++ demo/demo.tsx
@@ -1,16 +1,17 @@
 import {h, render} from "preact";
 
-import "prismjs/themes/prism.min.css";
+//import "prismjs/themes/prism.min.css";
 import "./style.less";
 
 import App from "./components/App";
 
+// eslint-disable-next-line
 declare const PPlayer: any;
 
 render(<App/>, document.body);
 
 window.addEventListener("load", () => {
-    if ('PPlayer' in window) {
+    if ("PPlayer" in window) {
         if (PPlayer.version != _VERSION_) {
             alert("Incompatible Penguin Player version, this demo is built for Penguin Player v" + _VERSION_ + ", but version " + PPlayer?.version + " is detected!");
         }
